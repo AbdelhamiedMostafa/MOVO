e6b6bc3d64dc9f9c68815af2d4d7902fd5e78151
Abdelhamied <midodesigner1@gmail.com>
2018-05-06 23:27:50 +0200

    adding join front end page

--- assets/js/join.js
+++ assets/js/join.js
@@ -1,3 +1,35 @@
+var login_tab = $('#login');
+var signup_tab = $('#signup');
+
+var login_form = $('#login-form');
+var signup_form = $('#signup-form');
+
+$( document ).ready(function functionName() {
+});
+
+login_tab.click(function functionName() {
+
+  if (!login_tab.hasClass("active-tab")) {
+    login_tab.toggleClass("active-tab");
+    signup_tab.toggleClass("active-tab");
+    login_form.toggle();
+    signup_form.toggle();
+  }
+
+
+});
+signup_tab.click(function functionName() {
+
+  if (!signup_tab.hasClass("active-tab")) {
+    login_tab.toggleClass("active-tab");
+    signup_tab.toggleClass("active-tab");
+    signup_form.toggle();
+    login_form.toggle();
+  }
+
+
+});
+
 $('.form').find('input, textarea').on('keyup blur focus', function (e) {
 
   var $this = $(this),
